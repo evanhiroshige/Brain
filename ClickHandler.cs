92d110ff17bc5cdeb9dca38d21527ca9f6b75c24
Yuxuan Liu <liu.yuxuan@northeastern.edu>
2018-12-04 15:51:51 -0500

    gestures

---
+++ ClickHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ClickHandler : MonoBehaviour, IInputClickHandler {
+
+    public GameObject text;
+
+    Color original;
+
+    public void Start()
+    {
+        original = this.GetComponent<Renderer>().material.color;
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+      
+
+        text.SetActive(!text.activeSelf);
+
+
+        Renderer r = this.GetComponent<Renderer>();
+
+        if (text.activeSelf)
+        {
+            r.material.SetColor("_Color", Color.blue);
+        } else
+        {
+            r.material.SetColor("_Color", original);
+        }
+
+    }
+
+}
